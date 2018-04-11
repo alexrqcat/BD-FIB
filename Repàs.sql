80003c4eaeb0d4ce2d664bf33544718834243ab2
alexrqcat <32525836+alexrqcat@users.noreply.github.com>
2018-04-11 21:50:50 +0200

    Exercicis de Repàs Select SQL

--- Repàs.sql
+++ Repàs.sql
@@ -5,23 +5,21 @@ from professors p, assignacions a, despatxos d
 where p.telefon is not null and p.sou > 2500 or
 p.telefon is null and p.dni = a.dni and a.modul = d.modul and a.numero = d.numero and d.superficie >= 20;
 
---EXERCICI 3--
+--EXERCICI 3---NOTA: 10--
 select distinct d.num_dpt, d.nom_dpt
 from departaments d, empleats e
 where e.num_dpt = d.num_dpt
 group by d.num_dpt, e.ciutat_empl
 having count(e.ciutat_empl) >= 2;
-------10-------
 
---EXERCICI 4--
+--EXERCICI 4---NOTA: 10--
 SELECT DISTINCT d.num_dpt, d.nom_dpt
 FROM departaments d NATURAL INNER JOIN empleats e
 WHERE NOT EXISTS (SELECT *
           FROM empleats e1
           WHERE e1.ciutat_empl <> 'MADRID' AND e1.num_dpt = d.num_dpt);
-------10-----
 
---EXERCICI 5--
+--EXERCICI 5---NOTA: 10--
 select distinct d.num_dpt, d.nom_dpt
 from departaments d, empleats e
 where d.num_dpt = e.num_dpt and exists (select *
@@ -29,7 +27,6 @@ where d.num_dpt = e.num_dpt and exists (select *
 		where e.ciutat_empl <> e1.ciutat_empl)
 		group by d.num_dpt
 		having count(e.ciutat_empl)>= 2;
------10-------
 
 --EXERCICI 6--
 select e.ciutat_empl
@@ -38,15 +35,14 @@ where not exists (select*
 			from departaments d
 			where e.ciutat_empl = d.ciutat_dpt);
 
---EXERCICI 7--
+--EXERCICI 7---NOTA: 10--
 select distinct d.num_dpt, d.nom_dpt
 from departaments d
 where not exists(select*
 	from empleats e
 	where e.num_dpt = d.num_dpt and e.ciutat_empl = 'MADRID');
--------10-------
 
---EXERCICI 8----
+--EXERCICI 8---NOTA: 10--
 select p.num_proj, p.nom_proj
 from projectes p, empleats e
 where p.num_proj = e.num_proj
@@ -54,28 +50,20 @@ group by p.num_proj
 having count(e.num_empl) >=2
 order by p.num_proj desc;
 
-----10------
-
---EXERCICI 9--
+--EXERCICI 9---NOTA: 10--
 select distinct e.nom_empl
 from empleats e
 where (select max(sou)
 		from empleats) = e.sou
 order by e.nom_empl desc;
 
-------10-------
-
---EXERCICI 10--
-
+--EXERCICI 10---NOTA: 10--
 select d.num_dpt, d.nom_dpt, avg(e.sou) as SOU
 from departaments d, empleats e
 where d.num_dpt = e.num_dpt and d.ciutat_dpt = 'MADRID'
 group by d.num_dpt;
 
----10---
-
----EXERCICI 11---
+---EXERCICI 11---NOTA: 10--
 select distinct d.num_dpt, d.nom_dpt
 from departaments d, empleats e
 where e.num_dpt = d.num_dpt and d.ciutat_dpt = 'MADRID' and e.sou > 200000;
----10---
